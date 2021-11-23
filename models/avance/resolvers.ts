6ae348c94c0c8271b89f1ea0467a92b59b5d5bcc
jarol <jaancastano@utp.edu.co>
2021-11-23 17:42:04 -0500

    corregir queries de avanceque no traian la info del proyecto

--- models/avance/resolvers.ts
+++ models/avance/resolvers.ts
@@ -3,18 +3,18 @@ import { ModeloAvance } from "./avance";
 const resolversAvance={
     Query:{
         Avances:async(parent,args)=>{
-            const avances=await ModeloAvance.find();
+            const avances=await ModeloAvance.find()..populate("proyecto").populate("creadoPor");
             return avances;
         },
     },
     Mutation:{
         crearAvance:async(parents,args)=>{
             const avanceCreado=ModeloAvance.create({
-                fecha:args.fecha,
+                fecha:new Date (args.fecha),
                 descripcion:args.descripcion,
                 proyecto:args.proyecto,
                 creadoPor:args.creadoPor,
-            });
+            })
             return avanceCreado;
         },
     },
